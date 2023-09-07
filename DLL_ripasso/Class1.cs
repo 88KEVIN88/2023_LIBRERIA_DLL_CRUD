43a80c964ed37fabe51f3b958d377f32c2138e11
azzol <azzol@PC001>
2023-09-07 18:03:23 +0200

    Add project files.

---
+++ DLL_ripasso/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL_ripasso
+{
+    public class Class1
+    {
+        public Random rand= new Random();
+        public void Add(bool fatto,string filecsv) //Funzione che aggiunge un campo alla fine del record
+        {
+            if(fatto == false)
+            {
+                string[] csvline = File.ReadAllLines(filecsv);
+
+                for(int i = 0; i < csvline.Length; i++) 
+                {
+                    csvline[i] += $";Mio valore:{rand.Next(10, 21)};0,";
+                }
+                fatto = true;
+                File.WriteAllLines(filecsv, csvline);
+
+            }
+        }
+    }
+}
